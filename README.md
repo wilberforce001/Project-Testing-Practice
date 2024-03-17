d3f59beb9a23d33ffad19e8afa06929b6f37f4cc
wilberforce001 <kenmisik@gmail.com>
2024-03-17 10:44:49 +0300

    Implemented reverse function that reverses the string

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ I then installed Webpack and Webpack CLI as developement dependencies by running
 
 The next step was to create a Webpack configuration file 'webpack.config.js' which defines how webpack bundles the assets of the project
 
+Installed Babel and necessary presets as dev dependencies
+
+Wrote describe function in my test file that performs capitalization
+
+Implemented reverse function that reverses the string 
---
+++ tests/index.test.js
@@ -0,0 +1,33 @@
+
+import { capitalize, reverseString} from '../src/index';
+
+
+describe('capitalize', () => {
+    test('capitalizes the first character of a string', () => {
+        expect(capitalize('hello world')).toBe('Hello world');
+    })
+    test('capitalizes a single character', () => {
+        expect(capitalize('a')).toBe('A');
+    })
+    test('works with empty string', () => {
+        expect(capitalize('')).toBe('');
+    })
+    test('does not modify already capitalized string', () => {
+        expect(capitalize('Hello')).toBe('Hello');
+    })
+});
+
+describe('reverseString', () => {
+    test('Reverses a common string', () => {
+        expect(reverseString('hello')).toBe('olleh');
+    })
+    test('capitalizes a single character', () => {
+        expect(reverseString('a')).toBe('a');
+    })
+    test('works with empty string', () => {
+        expect(reverseString('')).toBe('');
+    })
+    test('reverses correctly a string with punctuation', () => {
+        expect(reverseString('Hello!')).toBe('!olleH');
+    })
+});
